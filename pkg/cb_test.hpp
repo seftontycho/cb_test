395abc2eaf6d173c47e90eb63b084a0a377d6f2c
seftontycho <seftontycho@hotmail.co.uk>
2024-05-02 16:53:11 +0100

    init

---
+++ pkg/cb_test.hpp
@@ -0,0 +1,20 @@
+#ifndef CB_TEST_HPP
+#define CB_TEST_HPP
+
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+namespace ffi {
+
+extern "C" {
+
+void add(int a, void (*cb)(int));
+
+} // extern "C"
+
+} // namespace ffi
+
+#endif // CB_TEST_HPP
